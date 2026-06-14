73a72ecd1f5d9bc28d9a948c29e7208c91b967e8
Mateus Thives <mateus.thives@hotmail.com>
2026-06-14 19:42:33 -0300

    Adicionado módulos

--- ClubeDaLeitura.ConsoleApp/Utilidades/GeradorIds.cs
+++ ClubeDaLeitura.ConsoleApp/Utilidades/GeradorIds.cs
@@ -4,8 +4,15 @@ public static class GeradorIds
 {
     private static int contadorIdsCaixa = 1;
 
+    private static int contadorIdsRevista = 1;
+
     public static int ObterIdCaixa()
     {
         return contadorIdsCaixa++;
+    }
+
+    public static int ObterIdRevista()
+    {
+        return contadorIdsRevista++
     }
 }
